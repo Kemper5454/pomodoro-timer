f4d2b1cafd0b98253465ef5054ee1e038c7c3e91
Kemper5454 <delovoy_makar@mail.ru>
2025-09-20 13:47:40 +0300

    ч

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pomodoro-timer/', // имя твоего репозитория
+  base: '/pomodoro-timer/', // обязательно — имя репозитория
 })
